4db8892865918062b8dae32c4ef910cb81d562ae
Reema Almurayshid <85951306+RMUR99@users.noreply.github.com>
2024-05-10 01:05:07 +0300

    first push using shell script

---
+++ Push.sh
@@ -0,0 +1,7 @@
+﻿﻿#!/bin/bash
+# Ask the user for their name
+echo 'What is your message?'
+read message
+echo 'Adding message .......................'
+
+git add . && git commit -m"$message" && git push origin main
